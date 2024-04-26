f96654282e88a22561a0d1ba137afb0b4c043cf4
Chunming <chunming200@gmail.com>
2024-04-26 12:05:42 +0700

    UPDATE: Catalog

--- src/app/(categories)/categories/page.tsx
+++ src/app/(categories)/categories/page.tsx
@@ -1,11 +1,124 @@
+"use client";
+import { ButtonCategories } from "@/components";
 import ButtonCategorieslist from "@/components/Organisms/ButtonCategorieslist/ButtonCategorieslist";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const page = () => {
+  const [showMore, setShowMore] = useState(false);
+  const categories = [
+    {
+      href: "/categories/math",
+      icon: "/svg/Math.svg",
+      title: "Mathematic",
+      description: "Communicate with math lovers here!",
+    },
+    {
+      href: "/categories/physic",
+      icon: "/svg/Physic.svg",
+      title: "Physical",
+      description: "Communicate with physic lovers here!",
+    },
+    {
+      href: "/categories/chemistry",
+      icon: "/svg/Chemistry.svg",
+      title: "Chemistry",
+      description: "Communicate with chemistry lovers here!",
+    },
+    {
+      href: "/categories/biology",
+      icon: "/svg/Biology.svg",
+      title: "Biology",
+      description: "Communicate with bio lovers here!",
+    },
+    {
+      href: "/categories/khmer",
+      icon: "/svg/khmer.svg",
+      title: "Writing",
+      description: "Share your khmer writing here!",
+    },
+    {
+      href: "/categories/history",
+      icon: "/svg/History.svg",
+      title: "History",
+      description: "Get to know history together here!",
+    },
+    {
+      href: "/categories/english",
+      icon: "/svg/English.svg",
+      title: "English",
+      description: "Sharing your English learning tips here!",
+    },
+  ];
+  const displayedCategories = categories.slice(
+    0,
+    showMore ? categories.length : 3
+  );
+
+  const handleShowMore = () => {
+    setShowMore(!showMore);
+  };
   return (
-    <div>
-      <ButtonCategorieslist></ButtonCategorieslist>
-    </div>
+    <>
+      <div className="flex flex-col justify-start">
+        <div className="text-2xl font-semibold  text-[#343A40]">Categories</div>
+        <div className="flex flex-col">
+          <div className="flex flex-col">
+            <div className="text-lg pt-2 font-medium text-[#6C757D] opacity-[60%]">
+              Education
+            </div>
+
+            <div className="grid grid-cols-1 justify-start gap-4">
+              {displayedCategories.map((category) => (
+                <Link href={category.href} key={category.title}>
+                  <ButtonCategories
+                    icon={category.icon}
+                    title={category.title}
+                    description={category.description}
+                  />
+                </Link>
+              ))}
+            </div>
+
+            {categories.length > 3 && (
+              <button
+                className="mt-4 text-center bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md"
+                onClick={handleShowMore}
+              >
+                {showMore ? "Show Less" : "Show More"}
+              </button>
+            )}
+          </div>
+
+          <div className="text-lg pt-2 font-medium  text-[#6C757D] opacity-[60%]">
+            General Content
+          </div>
+          <div className="grid grid-cols-1 justify-start">
+            <Link href={"/categories/generalknowledge"}>
+              <ButtonCategories
+                icon="/svg/GeneralKnowledge.svg"
+                title="General Knowledge"
+                description="share your experiences here!"
+              ></ButtonCategories>
+            </Link>
+            <Link href={"/categories/mental"}>
+              <ButtonCategories
+                icon="/svg/Mental.svg"
+                title="Mental consultant"
+                description="sharing your mental problems here! we can help you with it."
+              ></ButtonCategories>
+            </Link>
+            <Link href={"/categories/technology"}>
+              <ButtonCategories
+                icon="/svg/Technology.svg"
+                title="Technology"
+                description="Communicate with Technology lovers here!"
+              ></ButtonCategories>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/Molecules/ButtonCategories/ButtonCategories.tsx
+++ src/components/Molecules/ButtonCategories/ButtonCategories.tsx
@@ -14,8 +14,8 @@ const ButtonCategories: React.FC<ButtonCategoriesProps> = ({
   return (
     <div className=" mt-2 ">
       <div className=" w-[250] lg:m-auto flex">
-        <button className=" flex-1 w-[123px] lg:w-[450px] lg:h-[100px] m-2 hover:bg-slate-200 h-[120px] px-[3px] flex items-center justify-between rounded-[16px] border border-gray-600">
-          <div className="lg:flex lg:items-center lg:space-x-[15px] lg:ml-2">
+        <button className=" flex-1 w-[123px] lg:w-[450px] lg:h-[100px] m-2 h-[120px] px-[3px] flex items-center justify-between rounded-[16px] border border-gray-200 max-sm:justify-center hover:bg-gray-50">
+          <div className="flex gap-4 max-sm:justify-center">
             <Image
               src={icon}
               alt="categories-icon"
@@ -26,23 +26,23 @@ const ButtonCategories: React.FC<ButtonCategoriesProps> = ({
             <div className="flex items-start justify-start flex-col">
               <Typography
                 fontSize="bold"
-                className=" mt-1 text-[#7B2CBF] text-xs lg:text-base w-full max-w-[270px] whitespace-nowrap line-clamp-1"
+                className=" mt-1 text-[#7B2CBF] text-sm lg:text-base w-full max-w-[270px] whitespace-nowrap line-clamp-1"
               >
                 {title}
               </Typography>
-              <Typography className="text-[10px] text-zinc-400">
+              <Typography className="text-[10px] text-zinc-500">
                 {description}
               </Typography>
             </div>
           </div>
-          <div className="pr-6">
+          <div className="pr-6 ">
             <svg
               width="24"
               height="24"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="hidden lg:block"
+              className="hidden sm:block"
             >
               <path d="M12 18L18 12L12 6" stroke="#222222" />
               <path d="M6 18L12 12L6 6" stroke="#222222" />
