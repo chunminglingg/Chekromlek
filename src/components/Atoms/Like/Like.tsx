8234e46f7f501998e1ee4b68e3b80a1b26d7992d
navin@1503 <79439418+khievnavin@users.noreply.github.com>
2024-04-26 13:26:08 +0700

    TEST: Testing

--- src/components/Atoms/Like/Like.tsx
+++ src/components/Atoms/Like/Like.tsx
@@ -1,6 +1,6 @@
 "use client"
-import React, { useState } from 'react';
 import Image from 'next/image';
+import React, { useState } from 'react';
 
 const Like = () => {
   const [isLiked, setIsLiked] = useState(false);
@@ -10,14 +10,6 @@ const Like = () => {
   };
 
   return (
-
-    <>
-      <button className='flex flex-row gap-1 justify-center' onClick={handleClick}>
-        <p className='font-normal text-[12px]'>{liked ? '13' : '12'}</p>
-        <Image src={liked ? "/card-svg/like/Afterlike.svg" : "/card-svg/like/like.svg"} alt={liked ? 'after-like' : 'like'} width={12} height={16} />
-      </button>
-    </>
-=======
     <button
       className={`flex flex-row gap-1 justify-center items-center ${
         isLiked ? 'text-[#E53E4F]' : 'text-[#343A40]'
@@ -25,14 +17,13 @@ const Like = () => {
       onClick={handleClick}
     >
       <p className={`text-[16px] max-sm:text-xs font-normal`}>{isLiked ? '13' : '12'}</p>
-      <img
+      <Image
         src={isLiked ? "/card-svg/like/Afterlike.svg" : "/card-svg/like/like.svg"}
         alt={isLiked ? 'after-like' : 'like'}
         width={16}
         height={16}
       />
     </button>
->>>>>>> 5c7792705014daa063d65ee1f89c49cb05e88785
   );
 };
 
